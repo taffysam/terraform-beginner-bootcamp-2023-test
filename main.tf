a1f49b3e2e0dc42eb017d4a2b494070e807e9c33
taffysam <tdzomba@gmail.com>
2023-09-27 13:50:26 +0000

    Creating Simple S3 Bucket

--- main.tf
+++ main.tf
@@ -1,12 +1,19 @@
 
 terraform {
   required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.18.1"
+    }
+  
     random = {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+  
   }
-}
+  }
+
 
 provider "random" {
   # Configuration options
@@ -15,10 +22,18 @@ provider "random" {
 resource "random_string" "bucket_name" {
   length  = 16
   special = false
-
 }
+
+resource "aws_s3_bucket" "example"{
+  bucket = "terraform-test-example-bucket"
+
+  tags = {
+    Name = "my-s3-bootcamp-bucket"
+    Environment = "Dev"
+  } 
+  }
 
 output "random_bucket_name"{
 value = random_string.bucket_name.result
-
 }
+
