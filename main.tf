1861269915ef127cfbf0ea2c79e27df3825cd61f
Tafadzwa D <42932105+taffysam@users.noreply.github.com>
2023-09-28 20:44:27 +0200

    30 re organise root module (#31)
    * Adding tf Files
    
    * Update on Providers

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 
 
+
 resource "aws_s3_bucket" "example" {
   bucket = "terraform-test-example-bucket"
 
   tags = {
-    Name        = "my-s3-bootcamp-bucket"
+    Name    = "my-s3-bootcamp-bucket"
     Environment = "Dev"
+    UserUuid    = var.user_uuid
   } 
-}
-
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
 }
