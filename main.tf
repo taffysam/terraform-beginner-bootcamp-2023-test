9918bf219218e28e5b55359e20581f712d7808ba
taffysam <tdzomba@gmail.com>
2023-09-28 18:39:51 +0000

    Updating Var file

--- main.tf
+++ main.tf
@@ -1,37 +1,5 @@
 
 
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.18.1"
-    }
-  
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-  }
-
-  cloud {
-    organization = "Tafadzwa"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
- 
-}
-
-provider "random" {
-  # Configuration options for the random provider, if needed.
-}
-
-resource "random_string" "bucket_name" {
-  length  = 16
-  special = false
-}
-
 resource "aws_s3_bucket" "example" {
   bucket = "terraform-test-example-bucket"
 
---
+++ providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.18.1"
+    }
+  
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+
+   cloud {
+   organization = "Tafadzwa"
+
+   workspaces {
+    name = "terra-house-1"
+   }
+  }
+ 
+}
+
+provider "random" {
+  # Configuration options for the random provider, if needed.
+}
+
+resource "random_string" "bucket_name" {
+  length  = 16
+  special = false
+}
