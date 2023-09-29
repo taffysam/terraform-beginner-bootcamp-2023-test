fe132e9f9120341b86bea95c4d0b01357a01366b
taffysam <tdzomba@gmail.com>
2023-09-29 12:11:22 +0000

    Commiting Module changes

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
 
-resource "aws_s3_bucket" "example" {
-  bucket = "terraform-test-example-bucket"
+#resource "aws_s3_bucket" "example" {
+#  bucket = "terraform-test-example-bucket"
+#
+#  tags = {
+#    Name    = "my-s3-bootcamp-bucket"
+#    Environment = "Dev"
+#    UserUuid    = var.user_uuid
+#  } 
+#}
 
-  tags = {
-    Name    = "my-s3-bootcamp-bucket"
-    Environment = "Dev"
-    UserUuid    = var.user_uuid
-  } 
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
-
--- modules/variables.tf
+++ modules/variables.tf
@@ -12,3 +12,18 @@ variable "user_uuid" {
     error_message = "User UUID must be in the format of a UUID (e.g., 123e4567-e89b-12d3-a456-426655440000)"
   }
 }
+
+variable "bucket_name" {
+  description = "The name of the bucket"
+  type        = string
+
+  validation {
+    condition = (
+      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 &&
+      can(regex("^[a-z0-9][-z0-9-.]*[-z0-9]$", var.bucket_name))
+    )
+    error_message = "The bucket name must be between 3 and 63 characters"
+    
+  }
+  
+}
