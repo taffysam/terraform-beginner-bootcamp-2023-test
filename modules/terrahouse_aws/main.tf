e0fbcee23a5d8dd89f945a68aa66a54f6c182f8a
Tafadzwa D <42932105+taffysam@users.noreply.github.com>
2023-09-29 16:20:19 +0200

    Taffysam/setting up s static website 34 (#38)
    * updating path
    
    * Updating module's main

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -12,27 +12,39 @@ terraform {
     }
   }
 
-
-resource "aws_s3_bucket" "my_website_bucket"{
-=======
-resource "aws_s3_bucket" "website_bucket"{
-
-  bucket = var.bucket-name
-  
-  tags = {
-    UserUuid = var.user_uuid
-  }
+#resource "aws_s3_bucket" "my_website_bucket"{
+#  bucket = var.bucket-name
+# 
+#  tags = {
+#    UserUuid = var.user_uuid
+#  }
+#}
 }
-}
-
-
-
-module "terrahouse_aws" {
-  source = "/modules/terrahouse_aws"
 
 module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
 
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
+}
+
+resource "aws_s3_bucket_website_configuration" "bootcamp_2023" {
+  bucket = aws_s3_bucket.bootcamp_2023.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  routing_rule {
+    condition {
+      key_prefix_equals = "docs/"
+    }
+    redirect {
+      replace_key_prefix_with = "documents/"
+    }
+  }
 }
